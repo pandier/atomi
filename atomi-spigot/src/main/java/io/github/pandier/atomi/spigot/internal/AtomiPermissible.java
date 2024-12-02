94318e7bd881a817ce007202bdaaba1b99bc0e3c
pandier <firegox@email.cz>
2024-12-02 20:28:54 +0100

    feat: Fully implement Spigot permissible

--- atomi-spigot/src/main/java/io/github/pandier/atomi/spigot/internal/AtomiPermissible.java
+++ atomi-spigot/src/main/java/io/github/pandier/atomi/spigot/internal/AtomiPermissible.java
@@ -2,37 +2,44 @@ package io.github.pandier.atomi.spigot.internal;
 
 import io.github.pandier.atomi.AtomiUser;
 import io.github.pandier.atomi.Tristate;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.permissions.PermissibleBase;
-import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.PermissionAttachment;
-import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.permissions.*;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.logging.Level;
 
 @ApiStatus.Internal
 public class AtomiPermissible extends PermissibleBase {
+    private final Player parent;
     private final AtomiUser user;
-    public final PermissibleBase previousPermissible;
+    private final List<PermissionAttachment> attachments = new ArrayList<>();
+    private final Map<String, PermissionAttachmentInfo> attachmentPermissions = new HashMap<>();
+    public final PermissibleBase replacedPermissible;
 
     AtomiPermissible(Player player, AtomiUser user, PermissibleBase oldPermissible) {
         super(player);
+        this.parent = player;
         this.user = user;
-        this.previousPermissible = oldPermissible;
+        this.replacedPermissible = oldPermissible;
     }
 
     @Override
     public boolean isPermissionSet(@NotNull String name) {
-        return user.permission(name) != Tristate.UNSET;
+        return attachmentPermissions.containsKey(name.toLowerCase(Locale.ROOT)) || user.permission(name) != Tristate.UNSET;
     }
 
     @Override
     public boolean hasPermission(@NotNull String name) {
+        String lowerName = name.toLowerCase(Locale.ROOT);
+        if (attachmentPermissions.containsKey(lowerName))
+            return attachmentPermissions.get(lowerName).getValue();
+
         return user.hasPermission(name);
     }
 
@@ -43,39 +50,96 @@ public class AtomiPermissible extends PermissibleBase {
 
     @Override
     public @NotNull PermissionAttachment addAttachment(@NotNull Plugin plugin) {
-        throw new UnsupportedOperationException(); // TODO
+        Objects.requireNonNull(plugin, "plugin");
+        if (!plugin.isEnabled())
+            throw new IllegalArgumentException("Plugin " + plugin.getDescription().getFullName() + " is disabled");
+        PermissionAttachment attachment = new PermissionAttachment(plugin, parent);
+        attachments.add(attachment);
+        recalculateAttachmentPermissions();
+        return attachment;
     }
 
     @Override
     public @Nullable PermissionAttachment addAttachment(@NotNull Plugin plugin, int ticks) {
-        throw new UnsupportedOperationException(); // TODO
+        PermissionAttachment attachment = addAttachment(plugin);
+
+        if (Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, attachment::remove, ticks) == -1) {
+            SpigotAtomiPlugin.atomi.plugin.getLogger().log(Level.SEVERE, "Failed to add permission attachment to " + parent + " for plugin " + plugin.getDescription().getFullName() + ": scheduler returned -1");
+            attachment.remove();
+            return null;
+        }
+
+        return attachment;
     }
 
     @Override
     public @NotNull PermissionAttachment addAttachment(@NotNull Plugin plugin, @NotNull String name, boolean value) {
-        throw new UnsupportedOperationException(); // TODO
+        Objects.requireNonNull(name, "name");
+        PermissionAttachment attachment = addAttachment(plugin);
+        attachment.setPermission(name, value);
+        return attachment;
     }
 
     @Override
     public @Nullable PermissionAttachment addAttachment(@NotNull Plugin plugin, @NotNull String name, boolean value, int ticks) {
-        throw new UnsupportedOperationException(); // TODO
+        Objects.requireNonNull(name, "name");
+        PermissionAttachment attachment = addAttachment(plugin, ticks);
+        if (attachment != null)
+            attachment.setPermission(name, value);
+        return attachment;
     }
 
     @Override
     public void removeAttachment(@NotNull PermissionAttachment attachment) {
-        throw new UnsupportedOperationException(); // TODO
+        Objects.requireNonNull(attachment, "attachment");
+
+        if (!attachments.remove(attachment))
+            throw new IllegalArgumentException("Attachment is not part of Permissible " + parent);
+
+        PermissionRemovedExecutor removalCallback = attachment.getRemovalCallback();
+        if (removalCallback != null)
+            removalCallback.attachmentRemoved(attachment);
+        recalculateAttachmentPermissions();
+    }
+
+    private void recalculateAttachmentPermissions() {
+        clearAttachmentPermissions();
+        for (PermissionAttachment attachment : attachments) {
+            iteratePermissions(attachment.getPermissions(), (name, value) -> {
+                attachmentPermissions.put(name.toLowerCase(Locale.ROOT), new PermissionAttachmentInfo(parent, name, attachment, value));
+                Bukkit.getServer().getPluginManager().subscribeToPermission(name, parent);
+            });
+        }
+    }
+
+    private void iteratePermissions(Map<String, Boolean> permissions, BiConsumer<String, Boolean> consumer) {
+        for (Map.Entry<String, Boolean> entry : permissions.entrySet()) {
+            consumer.accept(entry.getKey(), entry.getValue());
+
+            Permission permission = Bukkit.getPluginManager().getPermission(entry.getKey());
+            if (permission != null) {
+                iteratePermissions(permission.getChildren(), consumer);
+            }
+        }
     }
 
     @Override
     public void recalculatePermissions() {
+        recalculateAttachmentPermissions();
     }
 
     @Override
     public @NotNull Set<PermissionAttachmentInfo> getEffectivePermissions() {
-        return new HashSet<>(); // TODO
+        return Set.copyOf(attachmentPermissions.values());
+    }
+
+    private void clearAttachmentPermissions() {
+        attachmentPermissions.forEach((name, info) -> Bukkit.getPluginManager().unsubscribeFromPermission(info.getPermission(), parent));
+        attachmentPermissions.clear();
     }
 
     @Override
     public synchronized void clearPermissions() {
+        clearAttachmentPermissions();
     }
 }
