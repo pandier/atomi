b23e12d1fab2bf7d246ce348fbcf8c28f53e783e
pandier <firegox@email.cz>
2025-01-12 17:25:27 +0100

    feat: Use TEXT_COLOR option type instead of NAMED_TEXT_COLOR for COLOR option

--- atomi-core/src/main/java/io/github/pandier/atomi/AtomiOptions.java
+++ atomi-core/src/main/java/io/github/pandier/atomi/AtomiOptions.java
@@ -1,10 +1,10 @@
 package io.github.pandier.atomi;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 
 public final class AtomiOptions {
     public static final AtomiOption<Component> PREFIX = AtomiOption.of("prefix", AtomiOptionType.COMPONENT);
     public static final AtomiOption<Component> SUFFIX = AtomiOption.of("suffix", AtomiOptionType.COMPONENT);
-    public static final AtomiOption<NamedTextColor> COLOR = AtomiOption.of("color", AtomiOptionType.NAMED_TEXT_COLOR);
+    public static final AtomiOption<TextColor> COLOR = AtomiOption.of("color", AtomiOptionType.TEXT_COLOR);
 }
